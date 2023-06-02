0278dfd75d95779b9885f5ba904afbf56ddbd3a1
Abhay Nagaraj B R <abhay121191@gmail.com>
2023-06-02 13:30:37 -0500

    Update ClientApplication.java
    Refactor the client application to have a WebCLient configured to be used

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -1,76 +1,70 @@
 package com.example.client;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import java.util.Random;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.observation.DefaultMeterObservationHandler;
-import io.micrometer.observation.Observation;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
-import io.micrometer.tracing.handler.TracingAwareMeterObservationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.actuate.metrics.web.reactive.client.ObservationWebClientCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ClientApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
 
+	@Autowired
+	private ObservationWebClientCustomizer webClientCustomizer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
-	// tag::rest-template[]
-	// IMPORTANT! To instrument RestTemplate you must inject the RestTemplateBuilder
 	@Bean
-	RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+	public WebClient webClientFromBuilder(WebClient.Builder webClientFromBuilder) { // works: http client request metrics exposed
+		webClientCustomizer.customize(webClientFromBuilder);
+		return webClientFromBuilder
+			.baseUrl("http://localhost:6543")
+			.build();	
 	}
-	// end::rest-template[]
 
-	// tag::runner[]
 	@Bean
-	CommandLineRunner myCommandLineRunner(ObservationRegistry registry, RestTemplate restTemplate) {
-		Random highCardinalityValues = new Random(); // Simulates potentially large number of values
-		List<String> lowCardinalityValues = Arrays.asList("userType1", "userType2", "userType3"); // Simulates low number of values
+	CommandLineRunner myCommandLineRunnerWebClient(ObservationRegistry registry, @Qualifier("webClientPlainBuilder") WebClient wc) {
 		return args -> {
-			String highCardinalityUserId = String.valueOf(highCardinalityValues.nextLong(100_000));
-			// Example of using the Observability API manually
-			// <my.observation> is a "technical" name that does not depend on the context. It will be used to name e.g. Metrics
-			 Observation.createNotStarted("my.observation", registry)
-					 // Low cardinality means that the number of potential values won't be big. Low cardinality entries will end up in e.g. Metrics
-					.lowCardinalityKeyValue("userType", randomUserTypePicker(lowCardinalityValues))
-					 // High cardinality means that the number of potential values can be large. High cardinality entries will end up in e.g. Spans
-					.highCardinalityKeyValue("userId", highCardinalityUserId)
-					 // <command-line-runner> is a "contextual" name that gives more details within the provided context. It will be used to name e.g. Spans
-					.contextualName("command-line-runner")
-					 // The following lambda will be executed with an observation scope (e.g. all the MDC entries will be populated with tracing information). Also the observation will be started, stopped and if an error occurred it will be recorded on the observation
-					.observe(() -> {
-						log.info("Will send a request to the server"); // Since we're in an observation scope - this log line will contain tracing MDC entries ...
-						String response = restTemplate.getForObject("http://localhost:7654/user/{userId}", String.class, highCardinalityUserId); // Boot's RestTemplate instrumentation creates a child span here
-						log.info("Got response [{}]", response); // ... so will this line
-					});
-
+			commandLineArgsBuilder(wc, "webClient");
 		};
 	}
-	// end::runner[]
 
-	Random randomUserTypePicker = new Random();
-
-	private String randomUserTypePicker(List<String> lowNumberOfValues) {
-		return lowNumberOfValues.get(randomUserTypePicker.nextInt(2));
+	private void commandLineArgsBuilder(WebClient webClientObject, String fromWhichWebClient) {
+		Random highCardinalityValues = new Random(); // Simulates potentially large number of values
+		String highCardinalityUserId = String.valueOf(highCardinalityValues.nextLong(100_000));
+		log.info("from: " + fromWhichWebClient);
+		log.info("userId: " + highCardinalityUserId);
+		log.info("Will send a request to the server"); // Since we're in an observation scope - this log line will contain tracing MDC entries ...
+		JsonNode jsonNodeResponse = null;
+		Object response = webClientObject
+				.get()
+				.uri("http://localhost:7654/user/{userId}", String.class, highCardinalityUserId)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+		log.info("Got response [{}]", response); // ... so will this line
 	}
 }
+
+
+
